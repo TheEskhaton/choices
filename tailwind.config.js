bc8ccc0da50e29b502f2d50bf0eae63bb22d34fa
TheEskhaton <TheEskhaton@gmail.com>
2020-11-28 20:58:06 +0100

    added ping class

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,15 @@ module.exports = {
         "bg-indigo-700",
         "bg-purple-700",
 
+        "bg-pink-300",
+        "bg-gray-300",
+        "bg-red-300",
+        "bg-green-300",
+        "bg-yellow-300",
+        "bg-blue-300",
+        "bg-indigo-300",
+        "bg-purple-300",
+
         "text-pink-200",
         "text-gray-200",
         "text-red-200",
